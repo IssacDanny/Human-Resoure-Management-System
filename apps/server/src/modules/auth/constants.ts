0a86cf64d01c8f02e61641f19fd8db3309512605
DankChoir <timzek7@gmail.com>
2026-03-30 18:03:54 +0700

    edit(env): move JWT SECRET to .env

--- apps/server/src/modules/auth/constants.ts
+++ apps/server/src/modules/auth/constants.ts
@@ -1,6 +1,5 @@
 export class AuthConstants {
-  // TODO: Move this to process.env.JWT_SECRET in production!
   static readonly secret =
-    'DO_NOT_USE_THIS_VALUE_IN_PRODUCTION_OR_STERLING_WILL_BE_UPSET';
+    process.env.JWT_SECRET || 'dev_secret_only_for_local_use';
   static readonly expiresIn = '60m'; // Token validity
 }
